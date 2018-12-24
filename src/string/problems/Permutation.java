cb21a2bd199fa42e3d2a629392d20b209b005ad2
MdManiruzzaman <159manir@gmail.com>
2018-12-24 16:06:01 -0500

    Permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,16 +1,26 @@
 package string.problems;
-
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Permutation {
 
     public static void main(String[] args) {
-
         /*
          * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
          * Write Java program to compute all Permutation of a String
          *
          */
+         permutation(" ", "1234");
+         }
+         public static void permutation(String prefix, String string){
+         int n = string.length();
+
+         if(n==0){
+            System.out.println(prefix);
+         }else{
+            for(int i=0; i < n; i++){
+                permutation(prefix + string.charAt(i), string.substring(0, i) + string.substring(i+1,n));
+            }
+        }
     }
 }
