6379cf04181dc5a679b9c3ccba421ced7a10402c
MdManiruzzaman <159manir@gmail.com>
2018-12-24 14:38:38 -0500

    Find Missing Number Updated

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -22,7 +22,7 @@ public class Numbers {
 
 	public static void main(String[] args) throws Exception {
 		
-		int [] num = new int[1000000];
+		int [] num = new int[1000];
 		storeRandomNumbers(num);
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		//Selection Sort
@@ -36,9 +36,9 @@ public class Numbers {
 		int n = num.length;
 		randomize (num, n);
 		//Insertion Sort
-		algo.insertionSort(num);
-		long insertionSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+	//	algo.insertionSort(num);
+	//	long insertionSortExecutionTime = algo.executionTime;
+	//	System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
 
 		//By following above, Continue for rest of the Sorting Algorithm....
 
@@ -52,7 +52,7 @@ public class Numbers {
 	public static void storeRandomNumbers(int [] num){
 		Random rand = new Random();
 		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(1000000);
+			num[i] = rand.nextInt(1000);
 		}
 	}
 
--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -8,22 +8,19 @@ public class FindMissingNumber {
     public static void main(String[] args) {
         /*
          * If n = 10, then array will have 9 elements in the range from 1 to 10.
-         * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
+         * For example {10, 2, 1, 4, 9, 3, 7, 8, 6}. One number will be missing in array (5 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
-         System.out.println("Missing number is : " +findMissing(array,10));
-         }
-         public static int findMissing(int[]array, int n) {
-
-         int total = 0;
-         int sum = 0;
-         for(int k = 0; k < n; k++) {
-            sum += k;
-         }
-         for(int k = 0; k < array.length; k++) {
-            total += k;
-         }
-         return(sum - total);
+        int[] array = new int[]{10, 2, 1, 4, 9, 3, 7, 8, 6};
+        missingNumber(array, 10);
+        }
+         public static int missingNumber(int[] array, int n) {
+        int total = (n * (n + 1)) / 2;
+        int sum = 0;
+        for (int index : array) {
+            sum += index;
+        }
+        System.out.println("The missing number is : "+(total - sum));
+        return total - sum;
     }
 }
