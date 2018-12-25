bb918ea1386d604bd1e3dd3552fba9afe4fa23b1
MdManiruzzaman <159manir@gmail.com>
2018-12-24 19:09:07 -0500

    Datastructure Updated

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -1,7 +1,5 @@
 package datastructure;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,11 +11,11 @@ public static void main(String[] args) {
          */
         HashMap<Integer, String> map = new HashMap<Integer, String>();
         map.put(1, "NYC");
-        map.put(2, "LA");
-        map.put(3, "Denver");
+        map.put(2, "PA");
+        map.put(3, "CA");
         map.put(4, "Boston");
-        map.put(5, "San Jose");
-        map.put(6, "Seattle");
+        map.put(5, "NJ");
+        map.put(6, "Florida");
 
         for (Map.Entry<Integer, String> USA : map.entrySet()) {
                 System.out.println("Collection View of USA is " + USA.getKey() + ": " + USA.getValue());
--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,7 +12,35 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+		List<String> Australia=new ArrayList<String>();
+		Australia.add("Sydney");
+		Australia.add("Melbourne");
+		Australia.add("Perth");
 
+		List<String>USA=new ArrayList<String>();
+		USA.add("NYC");
+		USA.add("NJ");
+		USA.add("FL");
+
+		List<String>Canada=new ArrayList<String>();
+		Canada.add("Toronto");
+		Canada.add("Montreal");
+		Canada.add("Vencuver");
+
+		Map<String, List<String>> listOfCities = new HashMap<String, List<String>>();
+		listOfCities.put("Cities of Australia: ", Australia );
+		listOfCities.put("Cities of USA: ",USA);
+		listOfCities.put("Cities of Canada: ", Canada);
+
+		Iterator it=listOfCities.entrySet().iterator();
+
+		while(it.hasNext()){
+			System.out.println(it.next());
+
+		}
+		for(Map.Entry<String,List<String>> cityInfo:listOfCities.entrySet()){
+			System.out.println("\n"+cityInfo.getKey()+" "+cityInfo.getValue());
+		}
 	}
 
 }
