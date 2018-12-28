65d749f5deffb7e3ff5708b29452021f6ac12aec
MdManiruzzaman <159manir@gmail.com>
2018-12-28 17:06:13 -0500

    Algorithm

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -21,24 +21,24 @@ public class Numbers {
 	 */
 
 	public static void main(String[] args) throws Exception {
-		
+
 		int [] num = new int[1000];
 		storeRandomNumbers(num);
-		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+		//ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		//Selection Sort
 		Sort algo = new Sort();
 		algo.selectionSort(num);
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
+       /* connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
         List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
+        printValue(numbers);*/
 		int n = num.length;
 		randomize (num, n);
 		//Insertion Sort
-	//	algo.insertionSort(num);
-	//	long insertionSortExecutionTime = algo.executionTime;
-	//	System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+		algo.insertionSort(num);
+		long insertionSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
 
 		//By following above, Continue for rest of the Sorting Algorithm....
 
@@ -52,7 +52,7 @@ public class Numbers {
 	public static void storeRandomNumbers(int [] num){
 		Random rand = new Random();
 		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(1000);
+			num[i] = rand.nextInt(100);
 		}
 	}
 
--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -2,6 +2,8 @@ package algorithm;
 
 import org.testng.Assert;
 
+import java.util.Arrays;
+
 public class UnitTestSorting {
 
     /*
@@ -13,16 +15,79 @@ public class UnitTestSorting {
         //Create Sort object
         Sort sort = new Sort();
         //apply unsorted array to selectionSort.
-        sort.selectionSort(unSortedArray);
+
         //verify if the unsorted array is sorted by the selection sort algorithm.
         try {
-            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
-        }catch(Exception ex){
-            ex.getMessage();
+            int [] actual = sort.selectionSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Selection sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Selection sort unit test failed");
         }
 
         //Now implement Unit test for rest of the soring algorithm...................below
 
+        //verify if the unsorted array is sorted by the Insertion sort algorithm.
+        try {
+            int [] actual = sort.insertionSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Insertion sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Insertion sort unit test failed");
+        }
 
+        //verify if the unsorted array is sorted by the Bubble sort algorithm.
+        try {
+            int [] actual = sort.bubbleSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Bubble sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Bubble sort unit test failed");
+        }
+
+        //verify if the unsorted array is sorted by the Merge sort algorithm.
+        try {
+            int [] actual = sort.mergeSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Merge sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Merge sort unit test failed");
+        }
+
+        //verify if the unsorted array is sorted by the quick sort algorithm.
+        try {
+            int [] actual = sort.quickSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Quick sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Quick sort unit test failed");
+        }
+
+        //verify if the unsorted array is sorted by the heap sort algorithm.
+        try {
+            int [] actual = sort.heapSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Heap sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Heap sort unit test failed");
+        }
+
+        //verify if the unsorted array is sorted by the bucket sort algorithm.
+        try {
+            int [] actual = sort.bucketSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Bucket sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Bucket sort unit test failed");
+        }
+
+        //verify if the unsorted array is sorted by the shell sort algorithm.
+        try {
+            int [] actual = sort.shellSort(unSortedArray);
+            Assert.assertEquals(actual,sortedArray);
+            System.out.println("Shell sort unit test passed");
+        }catch(AssertionError ex){
+            System.out.println("Shell sort unit test failed");
+        }
     }
 }
